c68bdaad97e3f1f51e4689c6a488b2e1484a5558
Administrator <Administrator@172.31.16.138>
2023-07-13 07:58:25 +0000

    day4

---
+++ src/test/java/seleniumtesting/RedioCheckbox.java
@@ -0,0 +1,10 @@
+package seleniumtesting;
+
+public class RedioCheckbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/seleniumtesting/SelectorElement.java
@@ -0,0 +1,10 @@
+package seleniumtesting;
+
+public class SelectorElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
